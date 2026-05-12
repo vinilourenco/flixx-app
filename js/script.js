9f0fc22da45b33adcd7e5814d162245cabc302d6
Vinícius Lourenço <vinicius_lourenco@outlook.com>
2026-05-12 13:40:38 -0300

    feat: display popular movies

--- js/script.js
+++ js/script.js
@@ -1,5 +1,49 @@
 const global = {
     currentPage: window.location.pathname
+};
+
+async function displayPopularMovies() {
+    const {results} = await fetchAPIData('movie/popular');
+    
+    results.forEach(movie => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+                movie.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />` : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>`;
+
+        document.querySelector('#popular-movies').appendChild(div);
+    });
+}
+
+// Fetch Data From TMDB API
+async function fetchAPIData(endpoint) {
+    const API_KEY = '2e8557935b6b1ef01f3fc28941766138';
+    const API_URL = 'https://api.themoviedb.org/3/';
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+
+    const data = await response.json();
+
+    return data;
 }
 
 // Highlight Active Link
@@ -17,7 +61,7 @@ function init() {
     switch (global.currentPage) {
         case '/':
         case '/index.html':
-            console.log('Home');
+            displayPopularMovies();
             break;
         case '/shows.html':
             console.log('Shows');
